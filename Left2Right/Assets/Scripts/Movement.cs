654f9e2983aea5393082208a175cc349d6d4e9b9
Rockastorm <Jacobb.teamxstudios@gmail.com>
2025-09-30 16:22:02 -0400

    Movement

--- Left2Right/Assets/Scripts/Movement.cs
+++ Left2Right/Assets/Scripts/Movement.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    // Variables
+    public float Speed;
+
+    private Vector3 position;
+
+    public InputActionReference movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        position = movement.action.ReadValue<Vector2>() * Speed * Time.deltaTime;
+
+        transform.position += position;
+
     }
 }
--- Left2Right/Assets/Scripts/SM.cs
+++ Left2Right/Assets/Scripts/SM.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class SceneManager : MonoBehaviour
+public class SM : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
